eed310f4247dc2663491f678624a5ffbbe6bd619
BlajeAeron <agibskg@gmail.com>
2026-05-05 22:12:43 +0800

    Update database connection to use environment variables

--- db.php
+++ db.php
@@ -1,7 +1,13 @@
 <?php
-$conn = new mysqli("localhost", "root", "", "video_app");
+$conn = new mysqli(
+  getenv("mysql.railway.internal"),
+  getenv("root"),
+  getenv("MRuHglKVQsWhHDFROtCUHnWyBYAHrHmg"),
+  getenv("railway"),
+  getenv("3306")
+);
 
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+  die("Connection failed: " . $conn->connect_error);
 }
 ?>
