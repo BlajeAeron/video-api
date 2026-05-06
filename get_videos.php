a9ffcc6983aba3e7da48eb481e52c6d45995d281
BlajeAeron <agibskg@gmail.com>
2026-05-06 11:24:39 +0800

    Update get_videos.php

--- get_videos.php
+++ get_videos.php
@@ -7,6 +7,10 @@ include 'db.php';
 
 $result = $conn->query("SELECT * FROM videos");
 
+if (!$result) {
+    die("Query error: " . $conn->error);
+}
+
 $data = [];
 
 while ($row = $result->fetch_assoc()) {
